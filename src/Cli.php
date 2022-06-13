8810035802b47d5f8aa67b0d535dadaf7fd15e86
tamerlan volhin <wiceum@gmail.com>
2022-06-13 16:43:46 +0500

    fix brain-games

--- src/Cli.php
+++ src/Cli.php
@@ -3,7 +3,10 @@
 namespace Hexlet\Code\Cli;
 
 use function cli\line;
+use function cli\prompt;
 
 function greeting() {
     line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
